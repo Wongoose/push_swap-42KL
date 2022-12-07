fb697e5061dd8bfee8d48246ef5702b5f7f70040
Wongoose <wong.zhengxiang@gmail.com>
2022-12-07 18:55:07 +0800

    Removed error messages

--- src/utils/error.c
+++ src/utils/error.c
@@ -6,7 +6,7 @@
 /*   By: zwong <zwong@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 16:06:46 by zwong             #+#    #+#             */
-/*   Updated: 2022/12/06 15:41:41 by zwong            ###   ########.fr       */
+/*   Updated: 2022/12/07 17:58:06 by zwong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 void	err_exit(char *err)
 {
+	(void)err;
 	ft_putstr_fd("Error\n", 2);
-	perror(err);
 	exit(1);
 }
 
 void	putstr_err(char *err)
 {
+	(void)err;
 	ft_putstr_fd("Error\n", 2);
-	ft_putstr_fd(err, 2);
+	// ft_putstr_fd(err, 2);
 	exit(1);
 }
